28ebe6dcc0db309fb85dda7801a180f9d83e7148
leo-nicha <nicha_bz@hotmail.co.th>
2025-12-01 23:32:05 +0700

    bug fixed

--- app/lottoonline/page.tsx
+++ app/lottoonline/page.tsx
@@ -6,7 +6,7 @@ import ContentItem from "../components/cards/ContentItem";
 
 const CalanderList: React.FC = () => {
   const items = [
-    { title: 'เร็วๆ นี้', subtitle: '', imageUrl: '' },
+    { title: 'เร็วๆ นี้', subtitle: 'เร็วๆ นี้', imageUrl: 'https://res.cloudinary.com/dvv3wvgnt/image/upload/v1763563164/lottowinner_xve4xu.jpg' },
   ];
 
   const router = useRouter();
@@ -18,16 +18,20 @@ const CalanderList: React.FC = () => {
   };
 
   return (
-    <div className=" w-full p-2 grid grid-cols-1 lg:grid-cols-3 lg:gap-4 justify-items-center mx-auto max-w-6xl mt-25">
-      {items.map((item) => (
-        <ContentItem
-          key={item.title}
-          title={item.title}
-          subtitle={item.subtitle}
-          imageUrl={item.imageUrl}
-          onClick={() => handleItemClick(item.title)}
-        />
-      ))}
+    <div className="bg-gray-100 min-h-screen lg:flex lg:justify-center">
+      <div className="bg-gray-100 min-h-screen lg:min-w-screen">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-2 pl-2 pr-2 pt-4 mx-auto max-w-6xl">
+          {items.map((item) => (
+            <ContentItem
+              key={item.title}
+              title={item.title}
+              subtitle={item.subtitle}
+              imageUrl={item.imageUrl}
+              onClick={() => handleItemClick(item.title)}
+            />
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
